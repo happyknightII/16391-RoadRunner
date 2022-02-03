92fbee9d4bc7997638fa51f4c178f7843469e6c0
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-02-02 21:27:59 -0500

    New Trajectory
    Co-Authored-By: Shxl2 <92115922+Shxl2@users.noreply.github.com>

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -15,12 +15,64 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        Pose2d startPose = new Pose2d(-5, -45, Math.toRadians(110));
+        Pose2d startPose = new Pose2d(6.44, -63, Math.toRadians(90));
 
-        TrajectorySequence drive = new DriveShim(DriveTrainType.MECANUM, new Constraints(56, 60, Math.toRadians(330), Math.toRadians(110), 8.87), startPose).trajectorySequenceBuilder(startPose).setTangent(Math.toRadians(280))
-                .splineToSplineHeading(new Pose2d(20,-66,Math.toRadians(180)), Math.toRadians(0))
-                .splineToSplineHeading(new Pose2d(50,-66,Math.toRadians(180)), Math.toRadians(0))
+        Pose2d hubPose = new Pose2d(-5, -45, Math.toRadians(110));
+
+        Pose2d barrierPose = new Pose2d(20,-66,Math.toRadians(180));
+
+        TrajectorySequence drive = new DriveShim(DriveTrainType.MECANUM, new Constraints(56, 33, Math.toRadians(330), Math.toRadians(110), 8.87), startPose).trajectorySequenceBuilder(startPose).setTangent(Math.toRadians(280))
+                .addDisplacementMarker( 20, () -> System.out.println("robot.intake.outtake()"))    // Set Intake to eject cube for #1 approach
+                .addDisplacementMarker(152, () -> System.out.println("robot.intake.outtake()"))    // Set Intake to eject cube for #2 approach
+                .addDisplacementMarker(290, () -> System.out.println("robot.intake.outtake()"))
+                .addDisplacementMarker(437, () -> System.out.println("robot.intake.outtake()"))
+                .addDisplacementMarker(584, () -> System.out.println("robot.intake.outtake()"))
+
+                .lineToSplineHeading(hubPose).turn(0)                                                      // Starting point -> Hub
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED)"))
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #1
+                //.addDisplacementMarker(() -> System.out.println("robot.intake.intake()"))
+                .back(30)
+                //.addDisplacementMarker(() -> System.out.println("robot.intake.stop()"))
+                .forward(30)                                                                                // Warehouse -> Hub
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.TOP)"))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED)"))
+
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #2
+                .back(33)
+                //.addDisplacementMarker(() -> System.out.println("robot.intake.stop()"))
+                .forward(33)                                                                                // Warehouse -> Hub
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.TOP)"))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED)"))
+
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #3
+                .back(36)
+                //.addDisplacementMarker(() -> System.out.println("robot.intake.stop()"))
+                .forward(36)                                                                                // Warehouse -> Hub
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.TOP)"))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED)"))
+
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #4
+                .back(39)
+                //.addDisplacementMarker(() -> System.out.println("robot.intake.stop()"))
+                .forward(39)                                                                                // Warehouse -> Hub
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.TOP)"))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                //.addDisplacementMarker(() -> System.out.println("robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED)"))
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #5
+                //.addDisplacementMarker(() -> robot.intake.intake())
+                .back(41)
+                //.addDisplacementMarker(() -> robot.intake.stop())
                 .build();
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setDimensions(11, 15.5)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/competitioncode/autonomouscode/RedAroundShortv2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/competitioncode/autonomouscode/RedAroundShortv2.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.teamcode.drive.Constants;
 import org.firstinspires.ftc.teamcode.drive.hardware.Robot;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Autonomous(name = "Red Auto: Warehouse - Around - Right - Short v2", group = "Red")
 //@Disabled
@@ -46,45 +47,75 @@ public class RedAroundShortv2 extends AutonomousTemplate {
 
     public void regularAutonomous() {
         super.regularAutonomous();
+
         Pose2d startPose = new Pose2d(6.44, -63, Math.toRadians(90));
+
+        Pose2d hubPose = new Pose2d(-5, -45, Math.toRadians(110));
+
+        Pose2d barrierPose = new Pose2d(20,-66,Math.toRadians(180));
 
         robot.driveTrain.setPoseEstimate(startPose);
 
 
-        Trajectory startAuto = robot.driveTrain.trajectoryBuilder(startPose)
-                .addDisplacementMarker(18, () -> robot.intake.outtake())
-                .splineToSplineHeading(new Pose2d(-5, -45, Math.toRadians(110)), Math.toRadians(120))
-                .addDisplacementMarker(() -> robot.intake.stop())
-                .build();
+        TrajectorySequence auto = robot.driveTrain.trajectorySequenceBuilder(startPose)
+                .addDisplacementMarker( 20, () -> System.out.println("robot.intake.outtake()"))    // Set Intake to eject cube for #1 approach
+                .addDisplacementMarker(152, () -> System.out.println("robot.intake.outtake()"))    // Set Intake to eject cube for #2 approach
+                .addDisplacementMarker(294, () -> System.out.println("robot.intake.outtake()"))
+                .addDisplacementMarker(446, () -> System.out.println("robot.intake.outtake()"))
+                .addDisplacementMarker(608, () -> System.out.println("robot.intake.outtake()"))
 
-        Trajectory moveToWarehouse = robot.driveTrain.trajectoryBuilder(startAuto.end(), Math.toRadians(280))
-                .addDisplacementMarker(2, () -> robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED))
-                .splineToSplineHeading(new Pose2d(20,-66,Math.toRadians(180)), Math.toRadians(0))
+                .lineToSplineHeading(hubPose).turn(0)                                                      // Starting point -> Hub
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED))
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #1
                 .addDisplacementMarker(() -> robot.intake.intake())
-                .splineToSplineHeading(new Pose2d(50,-66,Math.toRadians(180)), Math.toRadians(0))
+                .back(30)
+                .addDisplacementMarker(() -> robot.intake.stop())
+                .forward(30)                                                                                // Warehouse -> Hub
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.TOP))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED))
+                .addDisplacementMarker(() -> robot.intake.stop())
+
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #2
+                .addDisplacementMarker(() -> robot.intake.intake())
+                .back(35)
+                .addDisplacementMarker(() -> robot.intake.stop())
+                .forward(35)                                                                                // Warehouse -> Hub
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.TOP))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED))
+                .addDisplacementMarker(() -> robot.intake.stop())
+
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #3
+                .addDisplacementMarker(() -> robot.intake.intake())
+                .back(40)
+                .addDisplacementMarker(() -> robot.intake.stop())
+                .forward(40)                                                                                // Warehouse -> Hub
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.TOP))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED))
+                .addDisplacementMarker(() -> robot.intake.stop())
+
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #4
+                .addDisplacementMarker(() -> robot.intake.intake())
+                .back(45)
+                .addDisplacementMarker(() -> robot.intake.stop())
+                .forward(45)                                                                                // Warehouse -> Hub
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.TOP))
+                .splineToSplineHeading(new Pose2d(-5,-45,Math.toRadians(110)), Math.toRadians(100)).turn(0)
+                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.INVERSE_FEED))
+
+                .splineToSplineHeading(barrierPose, Math.toRadians(0))                                      // Hub -> Warehouse #5
+                .addDisplacementMarker(() -> robot.intake.intake())
+                .back(50)
                 .addDisplacementMarker(() -> robot.intake.stop())
                 .build();
 
-        Trajectory moveToShippingHub = robot.driveTrain.trajectoryBuilder(moveToWarehouse.end(),Math.toRadians(180))
-                .splineToSplineHeading(new Pose2d(20,-66,Math.toRadians(180)), Math.toRadians(180))
-                .addDisplacementMarker(() -> robot.arm.setLevel(Constants.ArmPosition.TOP))
-                .splineToSplineHeading(new Pose2d(-5,-40,Math.toRadians(110)), Math.toRadians(100))
-                .addDisplacementMarker(() -> robot.intake.outtake())
-                .build();
-
-        robot.driveTrain.followTrajectory(startAuto);
-        cycleTimer.reset();
-        while (opModeIsActive()) {
-            robot.driveTrain.followTrajectory(moveToWarehouse);
-            if (cycleTimer.time() > cycleTime) {
-                cycleTime = cycleTimer.time();
-            }
-            cycleTimer.reset();
-            if (runTimer.time() + cycleTime > 29.5) {
-                break;
-            }
-            robot.driveTrain.followTrajectory(moveToShippingHub);
-        }
+        robot.driveTrain.followTrajectorySequence(auto);
     }
 }
 
